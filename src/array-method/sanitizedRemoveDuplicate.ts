e77e4c43ddc0c2179479d947c8681db6ce13ebe0
Jihad Hossain <78225332+jihad-hossain1@users.noreply.github.com>
2024-09-27 06:12:23 +0000

    add remove duplicate key

---
+++ src/array-method/sanitizedRemoveDuplicate.ts
@@ -0,0 +1,23 @@
+/**
+ * Sanitizes an array of objects by extracting the specified property values and returning them as an array of strings.
+ * 
+ * @param objectsArray - The array of objects to be sanitized.
+ * @param property - The property key to extract values from each object.
+ * @returns An array of strings extracted from the specified property.
+ */
+function toStringArray<T>(objectsArray: T[], property: keyof T): string[] {
+    return Array.from(
+      new Set(
+        objectsArray
+          .map((obj) => {
+            // Check if the property exists and is a string
+            const value = obj[property];
+            return typeof value === 'string' ? value : '';
+          })
+          .filter(Boolean) // Remove any empty or falsy values
+      )
+    );
+  }
+  
+  export default toStringArray;
+  
