5ce8a43d2a4546145399e9f7cc1bd2545e52f8ae
Jihad Hossain <78225332+jihad-hossain1@users.noreply.github.com>
2024-09-29 05:46:27 +0000

    removeduplicatecompare func add

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import findNonDuplicateItems from './array-method/findNonDuplicateItems.js';
-import removeDuplicates from './array-method/removeDuplicate.js';
+import {removeDuplicates, removeDuplicatesCompare} from './array-method/removeDuplicate.js';
 import { toStringArray, toObjectArray } from './array-method/sanitizedArray.js';
 import { formatKeys, formatKeysWithRemoveKeys } from './object-method/formatObject.js'
 import toStringArrayDuplicate from './array-method/sanitizedRemoveDuplicate.js'
 
 
-export { findNonDuplicateItems, removeDuplicates, toStringArray, toObjectArray, formatKeys, formatKeysWithRemoveKeys, toStringArrayDuplicate };
+export { findNonDuplicateItems, removeDuplicates,removeDuplicatesCompare, toStringArray, toObjectArray, formatKeys, formatKeysWithRemoveKeys, toStringArrayDuplicate };
