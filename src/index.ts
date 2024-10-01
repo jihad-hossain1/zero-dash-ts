551247899ea85e218739124f120dafc0a5419d0c
jihad-hossain1 <jihadkhan934@gmail.com>
2024-09-30 22:26:16 -0700

    replaceObjectKeys function added

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
-import findNonDuplicateItems from './array-method/findNonDuplicateItems.js';
-import {removeDuplicates, removeDuplicatesCompare} from './array-method/removeDuplicate.js';
-import { toStringArray, toObjectArray } from './array-method/sanitizedArray.js';
-import { formatKeys, formatKeysWithRemoveKeys } from './object-method/formatObject.js'
-import toStringArrayDuplicate from './array-method/sanitizedRemoveDuplicate.js'
-import {countDuplicatesAndUniqueByKey} from './array-method/findDuplicate.js'
-import { replaceKeysInArray } from './array-method/replaceObjectKeysOfArray.js';
+import findNonDuplicateItems from "./array-method/findNonDuplicateItems.js";
+import {
+    removeDuplicates,
+    removeDuplicatesCompare,
+} from "./array-method/removeDuplicate.js";
+import { toStringArray, toObjectArray } from "./array-method/sanitizedArray.js";
+import {
+    formatKeys,
+    formatKeysWithRemoveKeys,
+} from "./object-method/formatObject.js";
+import toStringArrayDuplicate from "./array-method/sanitizedRemoveDuplicate.js";
+import { countDuplicatesAndUniqueByKey } from "./array-method/findDuplicate.js";
+import { replaceKeysInArray } from "./array-method/replaceObjectKeysOfArray.js";
+import { replaceObjectKeys } from "./object-method/replaceKeysObject.js";
 
 
-export {replaceKeysInArray, findNonDuplicateItems, removeDuplicates,removeDuplicatesCompare,countDuplicatesAndUniqueByKey, toStringArray, toObjectArray, formatKeys, formatKeysWithRemoveKeys, toStringArrayDuplicate };
+
+
+
+
+
+
+export {
+    replaceKeysInArray,
+    findNonDuplicateItems,
+    removeDuplicates,
+    removeDuplicatesCompare,
+    countDuplicatesAndUniqueByKey,
+    toStringArray,
+    toObjectArray,
+    formatKeys,
+    formatKeysWithRemoveKeys,
+    toStringArrayDuplicate,
+    replaceObjectKeys,
+};
